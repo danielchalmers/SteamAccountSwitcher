2ce03c25cb6663173f37a239707e8a878d8f1597
Daniel Chalmers <MoonDPC@gmail.com>
2015-04-25 14:56:22 -0400

    Add account support

---
+++ SteamAccountSwitcher/AccountHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAccountSwitcher
+{
+	class AccountHandler
+	{
+		private List<Account> _accounts;
+
+		private AccountHandler()
+		{
+			_accounts = new List<Account>();
+		}
+
+		public static void LogOut()
+		{
+			Scripts.Run(Scripts.Close, new Account());
+		}
+
+		public static void LogIn(Account account)
+		{
+			Scripts.Run(Scripts.Open, account);
+		}
+	}
+}
--- SteamAccountSwitcher/Scripts.cs
+++ SteamAccountSwitcher/Scripts.cs
@@ -1,8 +1,29 @@
-﻿namespace SteamAccountSwitcher
+﻿using System.Diagnostics;
+using SteamAccountSwitcher.Properties;
+
+namespace SteamAccountSwitcher
 {
 	internal class Scripts
 	{
 		public const string Open = "start \"\" {0} -login \"{1}\" \"{2}\"";
 		public const string Close = "start \"\" {0} -shutdown";
+
+		public static void Run(string command, Account account)
+		{
+			var cmd = new ProcessStartInfo
+			{
+				FileName = "cmd.exe",
+				WindowStyle = ProcessWindowStyle.Hidden,
+				CreateNoWindow = true,
+				RedirectStandardInput = true,
+				UseShellExecute = false
+			};
+			var p = new Process { StartInfo = cmd };
+			p.Start();
+
+			using (var sw = p.StandardInput)
+				if (sw.BaseStream.CanWrite)
+					sw.WriteLine(command, Settings.Default.SteamPath, account.Username, account.Password);
+		}
 	}
 }
