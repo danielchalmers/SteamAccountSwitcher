a5791089cd53e938111188f978a2543b1870bdfd
Daniel Chalmers <MoonDPC@gmail.com>
2015-04-25 17:28:03 -0400

    Add context menu to buttons

--- SteamAccountSwitcher/AccountHandler.cs
+++ SteamAccountSwitcher/AccountHandler.cs
@@ -65,7 +65,8 @@ namespace SteamAccountSwitcher
 					Height = 32,
 					HorizontalContentAlignment = HorizontalAlignment.Left,
 					Margin = new Thickness(0, 0, 0, 8),
-					Padding = new Thickness(4,0,0,0)
+					Padding = new Thickness(4,0,0,0),
+					ContextMenu = DefaultMenu(),
 				};
 				btn.Click += Button_Click;
 				btn.MouseEnter += delegate{SelectedIndex = _stackPanel.Children.IndexOf(btn);};
@@ -77,6 +78,80 @@ namespace SteamAccountSwitcher
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			LogIn(_accounts[SelectedIndex]);
+		}
+
+		private void OpenPropeties()
+		{
+			var dialog = new AccountProperties(_accounts[SelectedIndex]);
+			dialog.ShowDialog();
+			_accounts[SelectedIndex] = dialog.NewAccount;
+			Refresh();
+		}
+
+		public void MoveUp(int i = -2)
+		{
+			var index = i == -2 ? SelectedIndex : i;
+			if (index == 0) return;
+			Swap(_accounts, index, index - 1);
+			Refresh();
+		}
+
+		public void MoveDown(int i = -2)
+		{
+			var index = i == -2 ? SelectedIndex : i;
+			if (_accounts.Count <= index + 1) return;
+			Swap(_accounts, index, index + 1);
+			Refresh();
+		}
+
+		public void Remove(int i = -2, bool msg = false)
+		{
+			var index = i == -2 ? SelectedIndex : i;
+			if (index < 0 || index > _accounts.Count - 1) return;
+			if (msg &&
+				Popup.Show($"Are you sure you want to remove this account?\r\n\r\n\"{_accounts[index].Username}\"",
+					MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes) == MessageBoxResult.No)
+				return;
+			_accounts.RemoveAt(index);
+			Refresh();
+		}
+
+		private static void Swap<T>(IList<T> list, int indexA, int indexB)
+		{
+			var tmp = list[indexA];
+			list[indexA] = list[indexB];
+			list[indexB] = tmp;
+		}
+
+		private ContextMenu DefaultMenu()
+		{
+			var contextMenu = new ContextMenu();
+
+			var item1 = new MenuItem { Header = "Properties..." };
+			item1.Click += delegate { OpenPropeties(); };
+
+			var item2 = new MenuItem { Header = "Move Up" };
+			item2.Click += delegate { MoveUp(); };
+
+			var item3 = new MenuItem { Header = "Move Down" };
+			item3.Click += delegate { MoveDown(); };
+
+			var item4 = new MenuItem { Header = "Remove..." };
+			item4.Click += delegate { Remove(-2, true); };
+
+			var item8 = new MenuItem { Header = "Exit" };
+			item8.Click += delegate { Application.Current.Shutdown(); };
+
+			contextMenu.Items.Add(item1);
+			contextMenu.Items.Add(new Separator());
+			contextMenu.Items.Add(item2);
+			contextMenu.Items.Add(item3);
+			contextMenu.Items.Add(new Separator());
+			contextMenu.Items.Add(item4);
+			contextMenu.Items.Add(new Separator());
+			contextMenu.Items.Add(item8);
+
+			return contextMenu;
 		}
 	}
 }
---
+++ SteamAccountSwitcher/Popup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SteamAccountSwitcher
+{
+	class Popup
+	{
+		public static MessageBoxResult Show(string text, MessageBoxButton btn = MessageBoxButton.OK,
+			MessageBoxImage img = MessageBoxImage.Information, MessageBoxResult defaultbtn = MessageBoxResult.OK)
+		{
+			return MessageBox.Show(text, "SteamAccountSwitcher", btn, img, defaultbtn);
+		}
+	}
+}
