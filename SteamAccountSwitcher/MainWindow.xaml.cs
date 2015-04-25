0c25ccd7dbb67fba36b796d2b3728ef33d7238a1
Daniel Chalmers <MoonDPC@gmail.com>
2015-04-25 14:29:50 -0400

    Change window properties

--- SteamAccountSwitcher/MainWindow.xaml.cs
+++ SteamAccountSwitcher/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,7 @@ namespace SteamAccountSwitcher
 		public MainWindow()
 		{
 			InitializeComponent();
+			Title = Assembly.GetExecutingAssembly().GetName().Name;
 		}
 	}
 }
