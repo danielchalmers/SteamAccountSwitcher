f0963b2853ee2ee9de95e28d25bbb7267e7e2304
Daniel Chalmers <MoonDPC@gmail.com>
2015-04-25 23:52:31 -0400

    Add options window

---
+++ SteamAccountSwitcher/Options.xaml.cs
@@ -0,0 +1,24 @@
+﻿#region
+
+using System.Windows;
+
+#endregion
+
+namespace SteamAccountSwitcher
+{
+	/// <summary>
+	///     Interaction logic for Options.xaml
+	/// </summary>
+	public partial class Options : Window
+	{
+		public Options()
+		{
+			InitializeComponent();
+		}
+
+		private void btnOK_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+	}
+}
