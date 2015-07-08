27eda8b5412218dc26b2d79a6a1371a4d223fdf4
Daniel Chalmers <moondpc@gmail.com>
2015-07-08 16:52:27 -0400

    Fix unhandled error causing crash

--- SteamAccountSwitcher/App.xaml.cs
+++ SteamAccountSwitcher/App.xaml.cs
@@ -12,12 +12,23 @@ namespace SteamAccountSwitcher
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Dispatcher.UnhandledException += OnDispatcherUnhandledException;
+        }
         protected override void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
 
             Settings.Default.Save();
             ClickOnceHelper.RunOnStartup(Settings.Default.AlwaysOn);
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            var errorMessage = $"An unhandled exception occurred:\n\n{e.Exception.Message}";
+            MessageBox.Show(errorMessage, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
         }
     }
 }
