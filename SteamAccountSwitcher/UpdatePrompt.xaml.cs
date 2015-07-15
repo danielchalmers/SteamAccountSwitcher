45ccea432b287b653dae70f7aed00e43680528bc
Daniel Chalmers <moondpc@gmail.com>
2015-07-14 21:12:26 -0400

    Change "UpdatePrompt" subtext for differing major or revision versions

--- SteamAccountSwitcher/UpdatePrompt.xaml.cs
+++ SteamAccountSwitcher/UpdatePrompt.xaml.cs
@@ -25,9 +25,31 @@ namespace SteamAccountSwitcher
         {
             InitializeComponent();
 
-            txtUpdateSubMsg.Text = !isRequired
-                ? ($"{AssemblyInfo.GetTitle()} {AssemblyInfo.GetVersionString(updateVersion)} is now available!\nDo you want to update to the latest version?")
-                : $"A mandatory update is available.\nPress \"{btnOK.Content}\" to update to the latest version.";
+            if (!isRequired)
+            {
+                if (updateVersion.Major != AssemblyInfo.GetVersion().Major)
+                {
+                    txtUpdateSubMsg.Text =
+                        $"A new major release, {AssemblyInfo.GetTitle()} {updateVersion.Major}.{updateVersion.Minor} is now available!";
+                }
+                else if (updateVersion.Revision != AssemblyInfo.GetVersion().Revision)
+                {
+                    txtUpdateSubMsg.Text =
+                        $"{AssemblyInfo.GetTitle()} {AssemblyInfo.GetVersionStringFull(updateVersion)} is now available!";
+                }
+                else
+                {
+                    txtUpdateSubMsg.Text =
+                        $"{AssemblyInfo.GetTitle()} {AssemblyInfo.GetVersionString(updateVersion)} is now available!";
+                }
+                txtUpdateSubMsg.Text += "\nDo you want to update to the latest version?";
+            }
+            else
+            {
+                txtUpdateSubMsg.Text =
+                    $"A mandatory update is available.\nPress \"{btnOK.Content}\" to update to the latest version.";
+            }
+
             btnNo.Visibility = isRequired ? Visibility.Hidden : Visibility.Visible;
             btnRemindLater.Visibility = isRequired ? Visibility.Hidden : Visibility.Visible;
             new ChangelogDownloader().GetChangelog(x => { txtChangelog.Text = x; });
