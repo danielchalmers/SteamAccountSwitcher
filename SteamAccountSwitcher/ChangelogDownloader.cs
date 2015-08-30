382de30f2bbe9b7fa0f355c206ca19b57292d0dc
Daniel Chalmers <MoonDPC@gmail.com>
2015-08-29 22:37:06 -0400

    Add changelog version dates

--- SteamAccountSwitcher/ChangelogDownloader.cs
+++ SteamAccountSwitcher/ChangelogDownloader.cs
@@ -58,7 +58,7 @@ namespace SteamAccountSwitcher
 
                         if (!versionDetected)
                             continue;
-                        str.AppendLine(isVersion ? $"{(firstVersion ? "" : Environment.NewLine)}{commit}" : $" {commit}");
+                        str.AppendLine(isVersion ? $"{(firstVersion ? "" : Environment.NewLine)}{commit} ({DateTime.Parse(j.commit.committer.date).ToString("yyyy-MM-dd")})" : $" {commit}");
                     }
                     str.Append($"\nYou can view the full changelog at {Resources.GitHubCommits}");
                     _updateText = str.ToString();
