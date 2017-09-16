99c3edcc1a5af8dc4de3fecb53649704369c8b4f
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-09-15 23:00:54 -0400

    Don't swallow popup exceptions

--- SteamAccountSwitcher/Popup.cs
+++ SteamAccountSwitcher/Popup.cs
@@ -7,14 +7,7 @@ namespace SteamAccountSwitcher
         public static MessageBoxResult Show(string text, MessageBoxButton button = MessageBoxButton.OK,
             MessageBoxImage image = MessageBoxImage.Information, MessageBoxResult defaultButton = MessageBoxResult.OK)
         {
-            try
-            {
-                return MessageBox.Show(text, AssemblyInfo.Title, button, image, defaultButton);
-            }
-            catch
-            {
-                return MessageBoxResult.Cancel;
-            }
+            return MessageBox.Show(text, AssemblyInfo.Title, button, image, defaultButton);
         }
     }
 }
