4f6f25751f164fb13746b5d477fde7847e8c71eb
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-11-14 19:32:30 -0500

    Replace tool button icons

--- SteamAccountSwitcher/Properties/Resources.Designer.cs
+++ SteamAccountSwitcher/Properties/Resources.Designer.cs
@@ -207,6 +207,26 @@ namespace SteamAccountSwitcher.Properties {
         /// <summary>
         ///   Looks up a localized string similar to The MIT License (MIT)
         ///
+        ///Copyright (c) 2016 MahApps
+        ///
+        ///Permission is hereby granted, free of charge, to any person obtaining a copy
+        ///of this software and associated documentation files (the &quot;Software&quot;), to deal
+        ///in the Software without restriction, including without limitation the rights
+        ///to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        ///copies of the Software, and to permit persons to whom the Software is
+        ///furnished to do so, subject to the following conditions:
+        ///
+        ///The above copyri [rest of string was truncated]&quot;;.
+        /// </summary>
+        public static string MahApps_Metro_IconPacks_License {
+            get {
+                return ResourceManager.GetString("MahApps_Metro_IconPacks_License", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to The MIT License (MIT)
+        ///
         ///
         ///Copyright (c) 2009 - 2014 Laurent Bugnion
         ///
