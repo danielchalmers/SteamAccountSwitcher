c77f22df12ff438f238b0db6b14437c6beed0342
Daniel Chalmers <daniel.chalmers@outlook.com>
2017-11-23 14:30:33 -0500

    Refactor account serialisation and deserialisation

--- SteamAccountSwitcher/SettingsHelper.cs
+++ SteamAccountSwitcher/SettingsHelper.cs
@@ -43,14 +43,33 @@ namespace SteamAccountSwitcher
 
         public static string SerializeAccounts(IEnumerable<Account> accounts)
         {
-            return new Encryption().Encrypt(JsonConvert.SerializeObject(accounts));
+            // Serialize account list to JSON.
+            var serialized = JsonConvert.SerializeObject(accounts);
+
+            // Obfuscate the serialized accounts.
+            var obfuscated = new Encryption().Encrypt(serialized);
+
+            // Return the obfuscated accounts.
+            // Deserialization must occur in reverse order (deobfuscate then deserialize).
+            return obfuscated;
         }
 
-        public static IEnumerable<Account> DeserializeAccounts(string data)
+        public static IEnumerable<Account> DeserializeAccounts(string accounts)
         {
-            return
-                JsonConvert.DeserializeObject<List<Account>>(new Encryption().Decrypt(data)) ??
-                new List<Account>();
+            // If the accounts data is empty, return a new account list.
+            if (string.IsNullOrEmpty(accounts))
+            {
+                return new List<Account>();
+            }
+
+            // Deobfuscate JSON accounts string.
+            var deobfuscated = new Encryption().Decrypt(accounts);
+
+            // Deserialize deobfuscated JSON string.
+            var deserialized = JsonConvert.DeserializeObject<List<Account>>(deobfuscated);
+
+            // If the deserialization was successful, return the result, otherwise return a new account list.
+            return deserialized ?? new List<Account>();
         }
 
         public static void OpenOptions()
