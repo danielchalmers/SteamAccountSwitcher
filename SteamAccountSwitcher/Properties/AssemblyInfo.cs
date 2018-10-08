9cffab6ae32d16cfe001706b12fc37243c4d3bf8
Daniel Chalmers <daniel.chalmers@outlook.com>
2018-10-07 22:14:57 -0500

    Update copyright year

--- SteamAccountSwitcher/Properties/AssemblyInfo.cs
+++ SteamAccountSwitcher/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Daniel Chalmers Software")]
 [assembly: AssemblyProduct("Steam Account Switcher")]
-[assembly: AssemblyCopyright("© Daniel Chalmers 2017")]
+[assembly: AssemblyCopyright("© Daniel Chalmers 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: Guid("22E1FAEA-639E-400B-9DCB-F2D04EC126E1")]
