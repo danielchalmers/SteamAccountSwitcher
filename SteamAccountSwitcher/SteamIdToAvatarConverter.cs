e3522b840c770d8132c3137fc10d78efb7f4bcf4
Daniel Chalmers <daniel.chalmers@outlook.com>
2022-11-06 12:00:06 -0600

    Show account avatars in menu
    Take 2 of 6eac75d2a46ab4665e7c733bcaa16db180ead54a
    
    Loaded from the Big Picture cache. If you don't use Big Picture, or use it rarely, they may not be up-to-date.

---
+++ SteamAccountSwitcher/SteamIdToAvatarConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media.Imaging;
+
+namespace SteamAccountSwitcher;
+
+[ValueConversion(typeof(object), typeof(object))]
+public class SteamIdToAvatarConverter : MarkupExtension, IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value is string id)
+		{
+			var steamDirectory = SteamClient.FindInstallDirectory();
+
+			if (steamDirectory != null)
+			{
+				var avatarPath = Path.Combine(steamDirectory, "config", "avatarcache", id + ".png");
+
+				if (File.Exists(avatarPath))
+					return new Image { Source = new BitmapImage(new Uri(avatarPath)) };
+			}
+		}
+
+		return null;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override object ProvideValue(IServiceProvider serviceProvider) => this;
+}
